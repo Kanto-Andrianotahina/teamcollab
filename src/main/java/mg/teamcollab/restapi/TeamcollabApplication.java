8b8fa00162a63a74f81db47372302c77cc7e759a
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2026-03-23 21:51:45 +0300

    init project

---
+++ src/main/java/mg/teamcollab/restapi/TeamcollabApplication.java
@@ -0,0 +1,13 @@
+package mg.teamcollab.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/teamcollab/restapi/TeamcollabApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.teamcollab.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
