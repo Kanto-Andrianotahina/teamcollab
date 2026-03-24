bc5a39f9062b38c89b6df255c35133b365a31532
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2026-03-24 17:27:56 +0300

    rectification entity project

--- src/main/java/mg/teamcollab/restapi/model/projects/Project.java
+++ src/main/java/mg/teamcollab/restapi/model/projects/Project.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -13,9 +14,13 @@ public class Project
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
     private String name;
+    @Setter
     private String description;
-    private Long owner;
+    @Setter
     private LocalDateTime createdAt;
+    @Setter
+    private Long ownerId;
 
 }
