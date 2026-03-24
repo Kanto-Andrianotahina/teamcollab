0582c95778c4ff77863fe178fbf9d1bca84d5f99
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2026-03-24 19:35:25 +0300

    Add setter for ProjectReadDTO

--- src/main/java/mg/teamcollab/restapi/dto/projects/ProjectReadDTO.java
+++ src/main/java/mg/teamcollab/restapi/dto/projects/ProjectReadDTO.java
@@ -1,10 +1,16 @@
 package mg.teamcollab.restapi.dto.projects;
 
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 public class ProjectReadDTO {
+    @Setter
     public String name;
+    @Setter
     public String description;
+    @Setter
     public Long owner;
+    @Setter
     public LocalDateTime createdAt;
 }
