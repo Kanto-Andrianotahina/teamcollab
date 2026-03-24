f4b91b905e0b85769c1188ccba2669bff30f847b
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2026-03-24 19:37:04 +0300

    add API GetMapping getAllProjects

--- src/main/java/mg/teamcollab/restapi/controller/projects/ProjectController.java
+++ src/main/java/mg/teamcollab/restapi/controller/projects/ProjectController.java
@@ -1,12 +1,12 @@
 package mg.teamcollab.restapi.controller.projects;
 
 import mg.teamcollab.restapi.dto.projects.ProjectCreateDTO;
+import mg.teamcollab.restapi.dto.projects.ProjectReadDTO;
 import mg.teamcollab.restapi.model.projects.Project;
 import mg.teamcollab.restapi.service.projects.ProjectService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
@@ -22,5 +22,10 @@ public class ProjectController {
             throw new Exception("Payload requis");
         }
         return projectService.createProject(dto);
+    }
+
+    @GetMapping
+    public List<ProjectReadDTO> getAllProjects() {
+        return projectService.findProjects();
     }
 }
