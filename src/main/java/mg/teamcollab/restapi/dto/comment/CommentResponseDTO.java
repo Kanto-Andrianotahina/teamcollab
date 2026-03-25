acca58f03b2f9a3345ad7429a887434d9c6d9a38
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2026-03-25 22:12:51 +0300

    Add DTO CommentResponseDTO

---
+++ src/main/java/mg/teamcollab/restapi/dto/comment/CommentResponseDTO.java
@@ -0,0 +1,13 @@
+package mg.teamcollab.restapi.dto.comment;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CommentResponseDTO {
+    private Long id;
+    private String content;
+    private Long authorId;
+    private LocalDateTime createdAt;
+}
