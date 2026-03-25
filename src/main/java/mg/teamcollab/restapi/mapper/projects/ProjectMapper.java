e3cd139fdf75163e3ec7c2385797497b162855df
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2026-03-25 20:57:25 +0300

    rectification project mapper

--- src/main/java/mg/teamcollab/restapi/mapper/projects/ProjectMapper.java
+++ src/main/java/mg/teamcollab/restapi/mapper/projects/ProjectMapper.java
@@ -1,17 +1,35 @@
 package mg.teamcollab.restapi.mapper.projects;
 
+import mg.teamcollab.restapi.dto.projectmembers.ProjectMemberReadDTO;
 import mg.teamcollab.restapi.dto.projects.ProjectReadDTO;
+import mg.teamcollab.restapi.mapper.projectmembers.ProjectMemberMapper;
 import mg.teamcollab.restapi.model.projects.Project;
+import mg.teamcollab.restapi.repository.projectmembers.ProjectMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ProjectMapper {
+    @Autowired
+    ProjectMemberRepository projectMemberRepository;
+    @Autowired
+    private ProjectMemberMapper projectMemberMapper;
     public ProjectReadDTO toDTO(Project p) {
         ProjectReadDTO dto = new ProjectReadDTO();
         dto.setName(p.getName());
         dto.setDescription(p.getDescription());
         dto.setOwner(p.getOwnerId());
         dto.setCreatedAt(p.getCreatedAt());
+
+        List<ProjectMemberReadDTO> members = projectMemberRepository
+                .findByProjectId(p.getId())
+                .stream()
+                .map(projectMemberMapper::toDTO)
+                .toList();
+
+        dto.setMembers(members);
         return dto;
     }
 }
